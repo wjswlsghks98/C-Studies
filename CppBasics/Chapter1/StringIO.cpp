5d288bf062331d668f5ff094c80832b2706f4593
jjh980928 <jordan98@kaist.ac.kr>
2023-03-18 01:19:59 +0900

    Studying Chapter1

---
+++ CppBasics/Chapter1/StringIO.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void)
+{
+    char name[100];
+    char lang[200];
+
+    std::cout << "What is your name? ";
+    std::cin >> name;
+
+    std::cout << "What is your favorite programming language? ";
+    std::cin >> lang;
+
+    std::cout << "My name is " << name << std::endl;
+    std::cout << "My favorite programming language is " << lang << std::endl;
+
+    return 0;
+} 
