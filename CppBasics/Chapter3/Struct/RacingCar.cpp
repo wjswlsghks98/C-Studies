d58cb9c5e2b0cc1ee8346ad24f3b41a55836d740
jjh980928 <jordan98@kaist.ac.kr>
2023-03-20 02:30:25 +0900

    Commit

---
+++ CppBasics/Chapter3/Struct/RacingCar.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+#define ID_LEN 20
+#define MAX_SPD 200
+#define FUEL_STEP 2
+#define ACC_STEP 10
+#define BRK_STEP 10
+
+struct Car
+{
+    char gamerID[ID_LEN];
+    int fuelGauge;
+    int curSpeed;
+};
+
+void ShowCarState(const Car &car)
+{
+    cout << "User ID: " << car.gamerID << endl;
+    cout << "Fuel Gauge: " << car.fuelGauge << "%" << endl;
+    cout << "Current Speed: " << car.curSpeed << "km/h" << endl << endl;
+}
+
+void Accel(Car &car)
+{
+    if(car.fuelGauge <= 0) return;
+    else car.fuelGauge -= FUEL_STEP;
+
+    if(car.curSpeed + ACC_STEP >= MAX_SPD)
+    {
+        car.curSpeed = MAX_SPD;
+        return;
+    }
+
+    car.curSpeed += ACC_STEP;
+}
+
+void Brake(Car &car)
+{
+    if(car.curSpeed < BRK_STEP)
+    {
+        car.curSpeed = 0;
+        return;
+    }
+    car.curSpeed -= BRK_STEP;
+}
+
+int main(void)
+{
+    Car run99 = {"run99", 100, 0};
+    Accel(run99);
+    Accel(run99);
+    ShowCarState(run99);
+    Brake(run99);
+    ShowCarState(run99);
+
+    Car sped77 = {"sped77", 100, 0};
+    Accel(sped77);
+    Brake(sped77);
+    ShowCarState(sped77);
+
+    return 0;
+}
