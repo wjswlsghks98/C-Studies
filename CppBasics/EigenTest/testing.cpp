f17bcb5bf6a637edd508578aaadc739826992099
김지효 <a04280905@gimjihyoui-MacBookPro-2.local>
2023-06-18 13:21:42 -0400

    Eigen Tutorials
    Committer: jinhwan wjswlsghks98@naver.com

--- CppBasics/EigenTest/testing.cpp
+++ CppBasics/EigenTest/testing.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
 
 int main(void)
 {
-    std::cout << "Hi" << std::endl;
+    // MatrixXd m(2,2);
+    // m(0,0) = 3;
+    // m(1,0) = 2.5;
+    // m(0,1) = -1;
+    // m(1,1) = m(1,0) + m(0,1);
     
+    // std::cout << m << std::endl;
+    MatrixXd m = MatrixXd::Random(3,3);
+    m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+    std::cout << "m = " << std::endl << m << std::endl;
+
+    Eigen::VectorXd v(3);
+    v << 1, 2, 3;
+    std::cout << "m * v = " << std::endl << m * v << std::endl;
+
+    // Testing various functions
+    m.resize(9,1);
+    std::cout << "Resized m = " << std::endl << m << std::endl;
 }
 
