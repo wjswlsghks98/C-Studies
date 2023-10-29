b049675b47d54edb61ce643e9b6634c340b78c57
jinhwan <wjswlsghks98@naver.com>
2023-10-29 15:06:37 +0900

    Chap13

---
+++ Chapter13/ClassTemplate/ArrayTemplate.h
@@ -0,0 +1,14 @@
+#ifndef __ARRAY_TEMPLATE_H__
+#define __ARRAY_TEMPlATE_H__
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+template <class T>
+class BoundCheckArray
+{
+    
+};
+
+#endif
