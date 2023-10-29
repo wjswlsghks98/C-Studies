70ea3d10a26e242071db875a8b2a286f8a15ce08
wjswlsghks98 <jinhwan0014@gmail.com>
2023-10-29 16:29:52 +0900

    Chap13

--- Chapter13/ClassTemplate/ArrayTemplate.h
+++ Chapter13/ClassTemplate/ArrayTemplate.h
@@ -8,7 +8,58 @@ using namespace std;
 template <class T>
 class BoundCheckArray
 {
-    
+    private:
+        T * arr;
+        int arrlen;
+
+        BoundCheckArray(const BoundCheckArray& arr) { }
+        BoundCheckArray& operator=(const BoundCheckArray& arr) { }
+    public:
+        BoundCheckArray(int len);
+        T& operator[] (int idx);
+        T operator[] (int idx) const;
+        int GetArrLen() const;
+        ~BoundCheckArray();
 };
+
+template <class T>
+BoundCheckArray<T>::BoundCheckArray(int len) : arrlen(len)
+{
+    arr = new T[len];
+}
+
+template <class T>
+T& BoundCheckArray<T>::operator[] (int idx)
+{
+    if (idx < 0 || idx >= arrlen)
+    {
+        cout << "Array index out of bound exception" << endl;
+        exit(1);
+    }
+    return arr[idx];
+}
+
+template <class T>
+T BoundCheckArray<T>::operator[] (ind idx) const
+{
+    if (idx < 0 || idx >= arrlen)
+    {
+        cout << "Array index out of bound exception" << endl;
+        exit(1);
+    }
+    return arr[idx];
+}
+
+template <class T>
+int BoundCheckArray<T>::GetArrLen() const
+{
+    return arrlen;
+}
+
+template <class T>
+BoundCheckArray<T>::~BoundCheckArray()
+{
+    delete []arr;
+}
 
 #endif
